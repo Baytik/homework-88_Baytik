5b39c98687a14d3ca315c81b14a8e060464cff17
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-03-19 17:32:49 +0600

    Сверстал Шапку

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Register from "./Components/Register/Register";
+import Login from "./Components/Login/Login";
+import Posts from "./Components/Posts/Posts";
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-
+          <Header/>
+          <Switch>
+              <Route path="/" exact component={Posts}/>
+              <Route path="/register" component={Register}/>
+              <Route path="/login" component={Login}/>
+          </Switch>
         </div>
     )
   }
