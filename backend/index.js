3cb6e2fc8713ccd85eb99e81cc06a6474940c257
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-03-19 12:00:23 +0600

    Сделал запросы на user

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const users = require('./app/users');
+/*const tasks = require('./app/tasks');*/
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const run = async () => {
+    await mongoose.connect('mongodb://localhost/posts', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
+    app.use('/users', users);
+    /*app.use('/tasks', tasks);*/
+    app.listen(port)
+};
+
+run().catch(e => {
+    console.error(e)
+});
