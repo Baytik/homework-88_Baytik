db32b108813fa91fccd1b46ef73a6bbf3d02361a
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-03-20 17:01:26 +0600

    Создал компонент imgThumbnail

--- frontend/src/Components/imgThumbNail/imgThumbNail.js
+++ frontend/src/Components/imgThumbNail/imgThumbNail.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {apiURL} from "../../apiURL";
-import ImageNot from './message.png';
 import './imgThimbNail.css';
+import imageNot from '../../message.png';
 
 const ImgThumbnail = props => {
-    let image = ImageNot;
-    console.log(image)
+    let image = imageNot;
     if (props.image) {
         image = apiURL + '/uploads/' + props.image;
+        if (image === 'http://localhost:8000/uploads/null') {
+            return <img src={imageNot} alt="post" className="image"/>
+        }
     }
-    return (
-        <img src={image} alt="" className="image"/>
-    );
+    return <img src={image} alt="post" className="image"/>
 };
 
 export default ImgThumbnail;
